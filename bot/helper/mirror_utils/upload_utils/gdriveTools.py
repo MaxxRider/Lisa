0f5b73015881e307f2ea00906ce0e2223612ea48
idsubhasish <95999921+idsubhasish@users.noreply.github.com>
2021-12-29 00:06:07 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -170,7 +170,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Subhasish-Mirror-Bot',
             'mimeType': mime_type,
         }
         if parent_id is not None:
